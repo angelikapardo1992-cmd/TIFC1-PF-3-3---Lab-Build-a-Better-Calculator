386c5fd362d15d043615e859d45bb5500e444865
angelikapardo1992-cmd <angelikapardo1992@gmail.com>
2026-02-14 00:13:55 -0500

    Implement calculator helper functions

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
+def addmultiplenumbers(numbers):
+  """Return the sum of a list of numbers."""
+  return sum(numbers)
+
+
+def multiplymultiplenumbers(numbers):
+  """Return the product of a list of numbers."""
+  result = 1
+  for number in numbers:
+    result *= number
+  return result
+
+
+def isitaninteger(num):
+  """Return True if num is an integer value, else False."""
+  if isinstance(num, bool):
+    return False
+  return isinstance(num, int) or (isinstance(num, float) and num.is_integer())
+
+
+def isiteven(num):
+  """Return True if num is an even integer, else False."""
+  return isitaninteger(num) and int(num) % 2 == 0
+
+
 def main():
   print("Hello learners!")
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
   main()
